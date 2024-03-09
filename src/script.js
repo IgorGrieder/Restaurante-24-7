5819b869bced30cf3f3ae2a6be2a709ab6948bd0
Igor Grieder Melo <igorgrieder@gmail.com>
2024-03-09 18:16:57 -0300

    Modificacao no design do site em relacao as fontes

--- src/script.js
+++ src/script.js
@@ -4,10 +4,15 @@ startPage()
 // Evets ---------------------------------------
 
 // Functions -----------------------------------
+
+// Function to start the page
 function startPage () {
   const menuInnerHTML = menu.map((item) => {
     // clone the model and configure it as it gets info of a fake API -> available.js
     const newItem = document.getElementById('menu-model').cloneNode(true)
+
+    // Making a reload animation
+    newItem.classList.add('animate-pulse')
 
     // Setting the item for the html
     newItem.querySelector('img').src = item.img
@@ -22,4 +27,17 @@ function startPage () {
   // Joins the array and adds it to the menuDisplay
   const display = menuInnerHTML.join('')
   menuDisplay.innerHTML = display
+
+  // Ending the animation after it finishes -> 2 secs
+  finishAnimation()
+}
+
+// Function to finish the reload animation
+function finishAnimation () {
+  // Iterates and removes the animation class
+  menuDisplay.querySelectorAll('#menu-model').forEach((item) => {
+    setTimeout(() => {
+      item.classList.remove('animate-pulse')
+    }, 2000)
+  })
 }
